c9884f0d9787da1f34da20f50c4e35524af84430
kambit <xoiiikoocoding@gmail.com>
2019-02-05 09:42:51 +0800

    Create test10.cs

---
+++ test10.cs
@@ -0,0 +1,12 @@
+using Neo.SmartContract.Framework;
+
+namespace Return42SmartContract
+{
+    public class Contract : SmartContract
+    {
+        public static int Main()
+        {
+            return 42;
+        }
+    }
+}
